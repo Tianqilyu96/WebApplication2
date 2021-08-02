acf56d3de0f558295588690f99e08680b9e40059
Tianqi Lyu <TianqiLyu@qq.com>
2021-08-03 00:06:06 +1000

    Add project files.

---
+++ Models/RecipesDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication4.Models
+{
+    public class RecipesDbContext : DbContext
+    {
+        public DbSet<Recipe> Recipes { get; set; }
+
+        public RecipesDbContext(DbContextOptions<RecipesDbContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
